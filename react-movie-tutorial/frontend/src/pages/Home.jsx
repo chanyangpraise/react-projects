aa76a666070ac83b4b235e530ecd101d49f34896
chanyangpraise <cychoi710@icloud.com>
2024-12-02 15:29:08 +0900

    ➕ Add react-router-dom

--- react-movie-tutorial/frontend/src/pages/Home.jsx
+++ react-movie-tutorial/frontend/src/pages/Home.jsx
@@ -2,14 +2,34 @@ import MovieCard from "../components/MovieCard";
 import { useState } from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "Dune", release_date: "2022" },
     { id: 3, title: "The Matrix", release_date: "2024" },
   ];
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("-----");
+  };
+
   return (
     <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
       <div className="movies-grid">
         {movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
